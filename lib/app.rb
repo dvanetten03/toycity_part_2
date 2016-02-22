629f046633fd9c2453c3c5d94b031036dfe65c1a
dvanetten03 <diane.vanetten@me.com>
2016-02-22 13:14:30 -0600

    Outline probably methods setup

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,15 @@
-require 'json'
-require 'artii'
+#-----------------------------
+# ToyCity Part 2
+# Diane Van Etten
+#-----------------------------
+
+require 'json'  #https://rubygems.org/gems/json
+require 'artii' #A small gem to generate ASCII art from text fed in to it https://rubygems.org/gems/artii
+
+	#Rubric requires that Udacity included code not be changed, so I commented it out
+	#path = File.join(File.dirname(__FILE__), '../data/products.json')
+	#file = File.read(path)
+	#products_hash = JSON.parse(file)
 
 def setup_files
 	path = File.join(File.dirname(__FILE__), '../data/products.json')
@@ -11,24 +21,65 @@ end
 def section_break
 	puts "-" * 30
 	puts "*" *30
+	puts ""
 end
 
-puts section_break
+def ascii_art(art_title)
+	a = Artii::Base.new :font => 'slant'
+	puts a.asciify(art_title)
+end
+
+def todays_date
+	puts "Today's Date: " + Time.now.strftime("%m/%d/%Y")
+	puts ""
+end
+
+# def toys_loop
+# 	$products_hash["items"].each do |toy|
+# end
+
+def toy_name
+end
+
+def products_section
+end
+
+def brands_section
+end
+
+def calculate_purchases
+end
+
+def calculate_sales
+end
+
+def calculate_avg_price
+end
+
+def calculate_avg_discount
+end
+
+def brand_name
+end
+
+def brands_toys
+end
+
+def sales_volume
+end
 
 # Print "Sales Report" in ascii art
-a = Artii::Base.new
-puts a.asciify('Sales Report')
+	ascii_art('Sales Report')
 
-puts section_break
+	section_break
 
 # Print today's date
-puts "Today's Date: " + Time.now.strftime("%m/%d/%Y")
-puts ""
+	todays_date
 
 # Print "Products" in ascii art
-puts a.asciify('Products')
+	ascii_art('Products')
 
-puts section_break
+	section_break
 
 # For each product in the data set:
 	# Print the name of the toy
@@ -39,7 +90,9 @@ puts section_break
 	# Calculate and print the average discount (% or $) based off the average sales price
 
 # Print "Brands" in ascii art
-puts a.asciify('Brands')
+	ascii_art('Brands')
+
+	section_break
 
 # For each brand in the data set:
 	# Print the name of the brand
